32d4449dab33982c0c550c49962e02231f791496
Romain <romainducarrouge@gmail.com>
2018-11-27 19:22:52 +0000

    log out all users

---
+++ logout_all_users.rb
@@ -0,0 +1,23 @@
+equire 'looker-sdk'
+
+# get API creds from environment variables
+LOOKER_ID = ENV['LOOKER_ID']
+LOOKER_SECRET = ENV['LOOKER_SECRET']
+LOOKER_PATH = 'https://COMPANY.looker.com:19999/api/3.1'
+
+looker = LookerSDK::Client.new(
+  :client_id => LOOKER_ID,
+  :client_secret => LOOKER_SECRET,
+  :api_endpoint => LOOKER_PATH
+)
+
+all_users = looker.all_users()
+count_users = 0
+all_users.each { |item|
+	if not item[:sessions][0].nil? 
+		looker.delete_user_session(item[:id], item[:sessions][0][:id])
+		puts "Logged out user: #{item[:first_name].to_s.capitalize} #{item[:last_name].to_s.capitalize}"
+		count_users += 1
+	end		
+}
+puts "\nScript logged out #{count_users} users"
